3443fed5a845badb47ab757c7499a3d9c47cf5da
AkshatGupta2009 <110603763+AkshatGupta2009@users.noreply.github.com>
2023-08-02 21:48:19 +1000

    Add files via upload

---
+++ newuibase.js
@@ -0,0 +1,32 @@
+var currentItem = "";
+const onPostSubmit = () => {
+    console.log(document.getElementById('postName').value);
+    console.log(document.getElementById('postDiscription').value);
+    console.log(document.getElementById('postType').value);
+    let name = document.getElementById('postName').value;
+    let discription = document.getElementById('postDiscription').value;
+    let type = document.getElementById('postType').value;
+    
+      // Create an object with the updated data
+      const data = {
+        name: name,
+        discription: discription,
+        type: type
+      };
+    
+    
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:3000/api/item");
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.setRequestHeader("Content-Type", "application/json");
+    
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          console.log(xhr.status);
+          console.log(xhr.responseText);
+          // Call the loadbooks function here if the update was successful
+        }
+      };
+      xhr.send(JSON.stringify(data));
+      location.reload();
+    }
