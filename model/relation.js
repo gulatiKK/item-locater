a5b01a75ec20fcdb8b970a6c09f9c055a94d7cb0
AkshatGupta2009 <110603763+AkshatGupta2009@users.noreply.github.com>
2024-01-28 15:55:37 +1100

    Update relation.js

--- model/relation.js
+++ model/relation.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 const relationSchema = new mongoose.Schema({
     item_name: {
         required: true,
-        type: String,
-        unique: true
+        type: String
     },
     container_name: {
         required: true,
         type: String
     }
-})
+});
 
-module.exports = mongoose.model('Relation', relationSchema)
+module.exports = mongoose.model('Relation', relationSchema);
